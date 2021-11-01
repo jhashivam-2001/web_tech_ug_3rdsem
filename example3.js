a68bc7b24edce375b891132cf181eb31bb9852c3
Shivam Jha <76417298+jhashivam-2001@users.noreply.github.com>
2021-11-01 22:08:30 +0530

    Add files via upload

---
+++ example3.js
@@ -0,0 +1,12 @@
+function un_defined() {
+  var a;
+  console.log(a);
+  a = "Before assigning value if we call a variable, it shows UNDEFINED.";
+  console.log(a);
+}
+function not_defined() {
+  console.log(
+    "When we did not declare the variable and try to call that variable, it shows NOT-DEFINED."
+  );
+  console.log(b);
+}
